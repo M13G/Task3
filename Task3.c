d6da155047bf8bc5d937b38bca4b4c3744a8d6a9
Reza <r2ambg@gmail.com>
2019-09-22 01:16:10 +0200

    Updated!

--- Task3.c
+++ Task3.c
@@ -1,29 +1,35 @@
 #include<stdio.h>
 #include<math.h>
-int main() {
+
+int main(){
   float a,b,c;
-  float delta,root1,root2;
+  float d,root1,root2;
+
 
   printf("Enter a, b and c of quadratic equation: ");
   scanf("%f%f%f",&a,&b,&c);
-  delta = (b*b)-(4*a*c);
-  if(delta<0){
-    printf("roots are complex number.\n");
 
-  }
-  if(delta==0){
-    printf("Both are equal.\n");
+  d = b * b - 4 * a * c;
 
-    root1 = -b/(2*a);
-    printf("Root of quadratic equation is : %f.3", root1);
-
+  if(d < 0){
+    printf("Roots are complex number.\n");
     return 0;
   }
-  if(delta>0){
-    root1 = (-b+sqrt(delta))/(2*a);
-    root2 = (-b-sqrt(delta))/(2*a);
-    printf("roots od equation are : &f.3, &f.3",root1, root2);
+  else if(d==0){
+   printf("Both roots are equal.\n");
 
+   root1 = -b /(2* a);
+   printf("Root of quadratic equation is: %.3f ",root1);
+
+   return 0;
   }
+  else{
+   printf("Roots are real numbers.\n");
+
+   root1 = ( -b + sqrt(d)) / (2* a);
+   root2 = ( -b - sqrt(d)) / (2* a);
+   printf("Roots of quadratic equation are: %.3f , %.3f",root1,root2);
+  }
+
   return 0;
 }
